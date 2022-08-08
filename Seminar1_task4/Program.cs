138726b7355092b2e25bfac1981f53d97b5fbf10
Alexei <vertuginalexei7@gmail.com>
2022-08-08 16:43:56 +0300

    seminarTask4

---
+++ Seminar1_task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое чсло");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = c;
+if(a>max)max=a;
+if(b>max)max=b;
+if(c>max)max=c;
+Console.WriteLine("max=");
+Console.WriteLine(max);
