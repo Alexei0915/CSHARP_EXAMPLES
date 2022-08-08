17eb1153a6526bb232ad91b0c9ab8fb464f1bc48
Alexei <vertuginalexei7@gmail.com>
2022-08-08 16:53:26 +0300

    seminarTask6

---
+++ Seminar1_task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n % 2==0)
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.Write("нечетное");
+}
