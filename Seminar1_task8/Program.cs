73abb751a3a63dd26bac23f84beda4dde8f7886f
Alexei <vertuginalexei7@gmail.com>
2022-08-08 17:14:18 +0300

    seminarTask8

---
+++ Seminar1_task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите  число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = n;i <=N;i++)
+if(i % 2==0)
+{
+    Console.Write(i);
+}
