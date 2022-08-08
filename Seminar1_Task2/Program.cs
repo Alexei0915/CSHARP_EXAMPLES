07a578c49c952e25b472dbb44fbf06957ef765d9
Alexei <vertuginalexei7@gmail.com>
2022-08-08 16:33:51 +0300

    seminarTask2

---
+++ Seminar1_Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое чсло");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int m = Convert.ToInt32(Console.ReadLine());
+int max = n;
+int min = m;
+if(n>max)max=n;
+if(m>max)max=m;
+Console.WriteLine("max=");
+Console.WriteLine(max);
