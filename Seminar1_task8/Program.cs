1f70a355453b952e96ad2d94239206049f38281b
Alexei <vertuginalexei7@gmail.com>
2022-08-11 16:03:16 +0300

    Seminar1task8

--- Seminar1_task8/Program.cs
+++ Seminar1_task8/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите  число");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите  число");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите  число");
 int N = Convert.ToInt32(Console.ReadLine());
