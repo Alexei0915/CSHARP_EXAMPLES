f3279a414fa34b9ea51865c62cdc251f4d984ea9
Alexei <vertuginalexei7@gmail.com>
2022-08-11 15:55:05 +0300

    Seminar2task10

--- Seminar2_task10/Program.cs
+++ Seminar2_task10/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите трехзначное число");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
 int a = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(a);
 Console.WriteLine("вторая цифра этого числа  " +stringNumber [1]);
