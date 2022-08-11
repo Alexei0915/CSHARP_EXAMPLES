9373f1b6ca7acd88f3e5ade60760418fd5d17b44
Alexei <vertuginalexei7@gmail.com>
2022-08-11 11:57:20 +0300

    Seminar2

---
+++ Seminar2_000/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(10, 100);
+
+Console.WriteLine("Случайное число от 10 до 99: "+a);
+
+int FirstDigit = a / 10;
+int SecondDigit = a % 10;
+
+if(FirstDigit>SecondDigit)
+{
+    Console.WriteLine("Наибольшая цифра: "+FirstDigit);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра: " + SecondDigit);
+}
