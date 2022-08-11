aeee5898af4fa0377bb43fae3dde8184c4ed4622
Alexei <vertuginalexei7@gmail.com>
2022-08-11 13:27:09 +0300

    Seminar2task14

---
+++ Seminar2_task14/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 7;
+int c = 23;
+
+if (a % b ==0 & a % c ==0)
+{
+    Console.WriteLine("кратное");
+    
+} 
+else
+{
+    Console.WriteLine("не кратно, остаток ");
+}
