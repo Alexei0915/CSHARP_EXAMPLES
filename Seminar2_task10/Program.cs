50265efb97bf6b27b272706882e8698b68df917a
Alexei <vertuginalexei7@gmail.com>
2022-08-11 14:21:19 +0300

    Seminar2task10

---
+++ Seminar2_task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(a);
+Console.WriteLine("вторая цифра этого числа  " +stringNumber [1]);
