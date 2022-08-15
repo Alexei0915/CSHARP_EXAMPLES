1dc81f421a4f819cc31c99b1985936213039d606
Alexei <vertuginalexei7@gmail.com>
2022-08-15 19:01:19 +0300

    Seminar3task22

---
+++ Seminar3_task22/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу которая принимает на вход число N  и выдает таблицу квадратов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int a =int.Parse(Console.ReadLine());
+int index = 1;
+while(index<=Math.Abs(a))
+{
+    Console.Write($" {index*index}, ");
+    index++;
+    
+}
