378cad819fc8f4dbfa3361ed546b2e04e257d400
Alexei <vertuginalexei7@gmail.com>
2022-08-15 18:26:44 +0300

    Seminar3task21

---
+++ Seminar3_task21/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу,которая принимает на вход координаты двух точек и находит растояние между ними в 2D пространстве.
+Console.WriteLine("введите А координаты");
+int ax =int.Parse(Console.ReadLine());
+int ay= int.Parse(Console.ReadLine());
+Console.WriteLine("введите B координаты");
+int bx =int.Parse(Console.ReadLine());
+int by= int.Parse(Console.ReadLine());
+
+Console.WriteLine(ax-bx);
+Console.WriteLine(ay-by);
+
+double AB = Math.Sqrt(Math.Pow (ax-bx,2) + Math.Pow (ay-by,2));
+
+Console.WriteLine(AB);
