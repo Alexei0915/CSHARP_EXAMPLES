217c79cbbf0825dba4d212b0a91aac28dc597b2f
Alexei <vertuginalexei7@gmail.com>
2022-08-19 19:44:14 +0300

    Seminar4task30

---
+++ Seminar4_task30/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу,которая выводит массив из 8 ми элементов
+//заполненный нулями и единицами в случайном порядке.
+int[] array = new int[8];
+int i = 0;
+while(i < array.Length )
+{
+    array[i]= new Random().Next(0, 2);
+    Console.Write (array[i]);
+    i++;
+   
+}
+
