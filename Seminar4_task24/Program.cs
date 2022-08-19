b896effb8279ea0bf29662cc2e8576c09fca5dba
Alexei <vertuginalexei7@gmail.com>
2022-08-19 14:11:11 +0300

    Seminar4task24

---
+++ Seminar4_task24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 24. Напишите программу,которая принимает на вход число (А)
+// и выдает сумму чисел от 1 до А. 
+
+Console.WriteLine("Введите число ");
+int A = int.Parse(Console.ReadLine());
+// int A;
+// try
+// {
+//    A = int.Parse(Console.ReadLine());
+// }
+// catch (FormatException ex)
+// {
+//     Console.WriteLine("Format " + ex.Message);
+//   return;
+// }
+// catch (OverflowException ex)
+// {
+//     Console.WriteLine("Overflow " + ex.Message);
+//     return;
+// }
+
+int sum = 0;
+for(int i = 1;i <=A; i++)
+{
+    sum = sum+=i;
+}
+
+Console.WriteLine($"{sum}");
