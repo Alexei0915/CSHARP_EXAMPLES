586a720a779b856bc1f013ee7fa8020acac6ae3a
Alexei <vertuginalexei7@gmail.com>
2022-08-19 18:28:28 +0300

    Seminar4task28

---
+++ Seminar4_task28/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу которая принимает на вход число N и выдает произведение чисел от 1 до N.
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+for (int i = 1; i <=N; i++)
+{
+    count= count * i;
+}
+Console.WriteLine(count);
