57200791710e8708f777dd3e13f74a7dca7e56b6
Alexei <vertuginalexei7@gmail.com>
2022-08-20 12:23:04 +0300

    Seminar4task27

---
+++ Seminar4_task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напиште программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN)
+  {
+    
+    int count = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
