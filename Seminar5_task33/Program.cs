52320a24303cc57a7938fec707c5ed9aab6ee6b4
Alexei <vertuginalexei7@gmail.com>
2022-08-21 17:21:47 +0300

    Seminar5task33

--- Seminar5_task33/Program.cs
+++ Seminar5_task33/Program.cs
@@ -37,3 +37,24 @@ if(var ==0)
 {
     Console.Write($"число не присутствует");
 }
+// Заготовка ввода массива.
+// void FillArray(int [] arr)
+// {
+//     int length= arr.Length;
+//     for (int i= 0;i <length;i++)
+//     {
+//         arr[i] = new Random().Next(-100,100);
+//     }
+// }
+// void PrintArray(int [] arr)
+// {
+//     int length= arr.Length;
+//     for (int i= 0;i < length ;i++)
+//     {
+//          Console.Write($"{arr [i]} ");
+//     }
+// }
+// int[] array = new int[5];
+
+// FillArray(array);
+// PrintArray(array);
