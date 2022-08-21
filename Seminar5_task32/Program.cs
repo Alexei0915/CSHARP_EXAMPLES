174676a3c42cd2eeec06d5c15809f141da7b2df7
Alexei <vertuginalexei7@gmail.com>
2022-08-21 16:39:59 +0300

    Seminar5task32

---
+++ Seminar5_task32/Program.cs
@@ -0,0 +1,29 @@
+﻿
+void FillArray(int [] arr)
+{
+    int length= arr.Length;
+    for (int i= 0;i <length;i++)
+    {
+        arr[i] = new Random().Next(-100,100);
+    }
+}
+void PrintArray(int [] arr)
+{
+    int length= arr.Length;
+    for (int i= 0;i < length ;i++)
+    {
+         Console.Write($"{arr [i]} ");
+    }
+}
+
+int[] array = new int[5];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(); // разделил по строчкам.
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= -array[i];// умножение на  "- 1"
+}
+PrintArray(array);
