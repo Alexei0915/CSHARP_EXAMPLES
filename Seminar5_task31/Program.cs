cadc7c96d3c613d319f9f19690ac99bad5cb1652
Alexei <vertuginalexei7@gmail.com>
2022-08-21 15:46:56 +0300

    Seminar5task31

---
+++ Seminar5_task31/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[12];
+int sumPositive = 0;
+int sumNegative = 0;
+int len =array.Length;
+int index = 0;
+while(index < array.Length)
+{
+    array[index] = new Random().Next(-9, 9);
+    index++;
+}
+for (int i = 0; i < len; i++)
+{
+    if(array[i] < 0)
+    {
+        sumNegative += array[i];
+    }
+    else
+    {
+        sumPositive += array[i];
+    }
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine($"сумма положительных {sumPositive}, сумма отрицательных {sumNegative}");
