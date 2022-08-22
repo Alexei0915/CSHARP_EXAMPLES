aac96a321d07737688aaf93d9087b792e7aa39e1
Alexei <vertuginalexei7@gmail.com>
2022-08-22 16:42:19 +0300

    Seminar5task37

---
+++ Seminar5_task37/Program.cs
@@ -0,0 +1,33 @@
+﻿//Найдите произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент,второй и предпоследний и т.д.Результат запишите в новом массиве.
+int [] array = {1,2,3,4,5};
+int len = array.Length;
+int index = 0;
+
+Console.Write("Массив = ");
+while (index < len)
+{
+    array [index] = new Random().Next(-10,10);
+    Console.Write(array[index]);
+    Console.Write(" ");
+    index ++;
+}
+Console.WriteLine();
+index= 0;
+if (len % 2 == 0)
+{
+    while (index <len / 2)
+    {
+        Console.Write(array[index]*array[len- index-1]+ " ");
+        index +=1;
+    }
+}
+else
+{
+   while(index< len/2)
+   {
+    Console.Write(array[index]* array[len - index-1]+" ");
+    index+=1;
+   }
+   Console.Write(array[index]);
+}
