61b1642d1d9dd35db6a9dc2c5926c727862b1839
Alexei <vertuginalexei7@gmail.com>
2022-08-25 14:13:17 +0300

    seminar6task40_42

---
+++ Seminar6_task40/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 40: Напишите программу,которая принимает на вход три числа
+// и проверяет,может ли существовать треугольник с сторонами такой длины.
+bool isTriangleExist(int A, int B, int C)
+{
+    if (A < B + C && B < A + C && C < B + A)
+    {
+        return true;
+    }
+    return false;
+}
+Console.WriteLine("Enter A");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter B");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter C");
+int C = int.Parse(Console.ReadLine()!);
+
+
+if (isTriangleExist(A, B, C))
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
---
+++ Seminar6_task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу,которая будет преобразовывать десятичное число в двоичное.
+int a = new Random().Next(0,100);
+
+void ToBin(int a)
+{
+    if(a > 1)
+    {
+        ToBin(a / 2);
+        Console.Write(a % 2);
+    }
+    else
+    {
+        Console.Write(a % 2);
+    }
+}
+Console.WriteLine("Ваше число - "+ a);
+
+Console.Write ("Ваше число в двоичной системе исчесления -");
+
+ToBin(a);
