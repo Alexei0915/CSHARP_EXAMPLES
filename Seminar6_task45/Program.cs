844606a9def75dc515f6cc0759554b22b21bdee6
Alexei <vertuginalexei7@gmail.com>
2022-08-25 14:54:03 +0300

    seminar6task45

---
+++ Seminar6_task45/Program.cs
@@ -0,0 +1,32 @@
+﻿// 
+Console.Write ("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine());
+int [] array = new int [length];
+
+for (int i = 0;i < array.Length;i++)
+{
+    array [i] = new Random().Next(100, 1000);
+}
+
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length;i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+
+int [] ArrayCopy (int[] array)
+{
+    int[]ResArr = new int[length];
+    for(int i = 0; i< length;i++)
+    {
+        ResArr[i] = array [i];
+    }
+    return ResArr;
+}
+Console.Write("Массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.Write("Копия массива: ");
+PrintArray(ArrayCopy(array));
