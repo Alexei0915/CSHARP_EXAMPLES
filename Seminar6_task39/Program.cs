68138f63b9a845c91b2a72f48823f3e8c4d9de20
Alexei <vertuginalexei7@gmail.com>
2022-08-25 12:59:24 +0300

    seminar6task39

---
+++ Seminar6_task39/Program.cs
@@ -0,0 +1,20 @@
+﻿//
+int[] ReverseArray(int[] input)
+{
+    int[] result = new int[input.Length];
+    for (int i = 0,j = input.Length - 1; i < input.Length;i++,j--)
+    {
+        result[i]= input [j];
+    }
+    return result;
+}
+
+void PrintArray(int[]input)
+{
+    for(var i = 0;i < input.Length; i++)
+    Console.Write (input[i]+ ",");
+}
+
+int [] input = new int[]{1, 2, 3, 4, 5};
+var result = ReverseArray(input);
+PrintArray(result);
