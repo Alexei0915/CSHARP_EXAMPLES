1dd329082de1f6f818d00ef8f5273e89d1e9b1e8
Alexei <vertuginalexei7@gmail.com>
2022-08-29 17:43:51 +0300

    seminar6task51

---
+++ Seminar7_task51/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 51 Задайте двумерный массив. Найдите сумму элементов находящуюся на главной диагонали.
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+FillArray(array);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write ($"[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write ($"{matr[i, j]} , ");
+        }
+        Console.WriteLine($"{matr [i, matr.GetLength(1)- 1]}]");
+        Console.WriteLine();
+    }
+}
+
+int temp = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (i == j)temp = temp + array[i,j];
+            
+    }
+}
+
+
+
+Console.WriteLine($"сумма диагонали  {temp}");
+PrintArray(array);
