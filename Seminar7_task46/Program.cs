c952defd3a23a728cf52a6226fae8a52c5ad86a0
Alexei <vertuginalexei7@gmail.com>
2022-08-29 14:26:14 +0300

    seminar6task46

---
+++ Seminar7_task46/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 46: Задайте двумерный массив размером m X n , заполненный случайными целыми числами.
+// m =3,n=4
+Console.Write("Введите высоту матрицы: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите длину матрицы: ");
+int n = int.Parse(Console.ReadLine());
+
+int [,] array = new int [m,n];
+
+void FillArray (int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1);j++)
+        {
+            matr [i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void Print (int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1);j++)
+        {
+            Console.Write(matr [i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+ 
+FillArray(array);
+Print (array);
