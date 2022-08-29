f39c157a6d1e0137424edc16cf6a77ae597e3651
Alexei <vertuginalexei7@gmail.com>
2022-08-29 14:56:07 +0300

    seminar6task48_

--- Seminar7_task48/Program.cs
+++ Seminar7_task48/Program.cs
@@ -6,20 +6,6 @@ Console.Write("Введите n : ");
 int n = int.Parse(Console.ReadLine());
 
 int[,] array = new int[m, n];
-
-// int[,] array = new int[m, n];
-
-// void FillArray(int[,] matr)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             matr[i, j] = new Random().Next(1, 10);
-//         }
-//     }
-// }
-
 void PrintArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
@@ -32,9 +18,9 @@ void PrintArray(int[,] arr)
     }
 }
 for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        array [i, j] = i + j;
-    }
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = i + j;
+}
 
-PrintArray (array);
+PrintArray(array);
