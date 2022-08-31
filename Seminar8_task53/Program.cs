9d9d25869cd539740f21e72540e1d0356ae3a83c
Alexei <vertuginalexei7@gmail.com>
2022-08-31 16:34:44 +0300

    seminar8task55

--- Seminar8_task53/Program.cs
+++ Seminar8_task53/Program.cs
@@ -1,39 +1,6 @@
 ﻿// Задача 53: Задайте двумерный массив.
 //Напишите программу,которая поменяет местами первую и последнюю строку массива.
-// Console.Write("Введите m: ");
-// int m = int.Parse(Console.ReadLine());
-// Console.Write("Введите n: ");
-// int n = int.Parse(Console.ReadLine());
 
-// int[,] array = new int[m, n];
-
-// void FillArray(int[,] matr)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             matr[i, j] = new Random().Next(1, 10);
-//         }
-//     }
-// }
-// FillArray(array);
-
-// void PrintArray(int[,] matr)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         Console.WriteLine();
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             Console.Write($"{matr[i, j] + " "}");
-//         }
-//         Console.WriteLine($"{matr[i, matr.GetLength(1) - 1]}");
-//         Console.WriteLine();
-//     }
-// }
-
-// PrintArray(array);
 int m = Convert.ToInt32(new Random().Next(3, 5));// randomly select how many rows wil be in array (случайным образом выберите, сколько строк будет в массиве)
 int n = m + Convert.ToInt32(new Random().Next(3, 5));// randomly select how many colums wil be in array,wich is larger that m (случайным образом выберите, сколько столбцов будет в массиве,который больше m)
 Console.WriteLine();
