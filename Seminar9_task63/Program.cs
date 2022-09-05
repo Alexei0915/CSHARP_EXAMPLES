af9af9a09a8efdb4a3564534fbbff1d8b96c2dec
Alexei <vertuginalexei7@gmail.com>
2022-09-05 14:07:37 +0300

    seminar9task63

---
+++ Seminar9_task63/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 63: Задайте значение N.Напишите программу,которая выведет все натуральные числа в промежутке от 1 до N.
+// Задача 65: Задайте значение M и N . Напишите программ,которая выведет все натуральные числа в промежутке от M до N.
+Console.WriteLine("text");
+int n = int.Parse(Console.ReadLine());
+void RecursionNaumbers(int i)
+{
+    if (i == n)
+    {
+        Console.Write($" {i}");
+    }
+    else 
+    {
+       
+        Console.Write($" {i}");
+        i++;
+        RecursionNaumbers(i);
+    }
+}
+
+int j = 1;
+
+RecursionNaumbers(j);
