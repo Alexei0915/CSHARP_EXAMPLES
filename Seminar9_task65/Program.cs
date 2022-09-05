6c77cf3b3dde8b41c6bfd91715f05f746927db29
Alexei <vertuginalexei7@gmail.com>
2022-09-05 14:15:47 +0300

    seminar9task65

---
+++ Seminar9_task65/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 65: Задайте значение M и N . Напишите программ,которая выведет все натуральные числа в промежутке от M до N.
+Console.WriteLine("Начало диапозона");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Конец диапозона");
+int n = int.Parse(Console.ReadLine());
+void RecursionNaumbers(int i)
+{
+   
+    if (i == n)
+    {
+        Console.Write($" {i}");
+    }
+    else 
+    {
+       
+        Console.Write($" {i}");
+        i++;
+        RecursionNaumbers(i);
+    }
+}
+
+int j = m;
+
+RecursionNaumbers(j);
